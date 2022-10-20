f743e715de144abbb14bbea0424c21477232f3fc
tobi <olufemioluwatobi1996@gmail.com>
2022-10-20 22:37:08 +0100

    fix store error

--- interfaces/store.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { ThunkDispatch } from 'redux-thunk';
-import store from '../store';
-
-export type RootState = ReturnType<typeof store.getState>;
-
-export type TDispatch = ThunkDispatch<RootState, void, any>;
-
-export type ThunkActionResponse = Promise<void | Record<string, unknown>> | undefined;
