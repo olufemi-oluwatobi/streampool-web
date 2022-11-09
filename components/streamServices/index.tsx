31805ac287b779ad6654b0a92f6ee5fbb58690b0
unknown <olufemioluwatobi1996@gmail.com>
2022-11-10 00:12:33 +0100

    remove alert

--- components/streamServices/index.tsx
+++ components/streamServices/index.tsx
@@ -173,7 +173,6 @@ const Index = () => {
           category={category}
           onClick={(service) => {
             setStreamService(service);
-            alert(`is it really mobile: ${isMobile}`);
             if (isMobile) router.push(`/stream_service/${service.id}`);
           }}
         />
