9cc403c6b759fe3d16ed9117cc44d89700051ce0
unknown <olufemioluwatobi1996@gmail.com>
2022-11-14 11:35:50 +0100

    streamcel fix auth data refresh

--- services/api/new-index.ts
+++ services/api/new-index.ts
@@ -1,34 +1,32 @@
 import axios, { Axios, AxiosRequestHeaders } from "axios";
 
-
 class BaseApi {
-    request: Axios
-    headers: AxiosRequestHeaders
-    loadRequest() {
-        const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-        this.loadHeaders()
-        this.request = axios.create({
-            baseURL: baseUrl,
-            headers: this.headers
-        });
-    }
-    loadHeaders() {
-        try {
-            if (this.headers) return this.headers;
-            const headers: AxiosRequestHeaders = {};
-            if ((typeof window !== 'undefined')) {
-                const authDataSerialized = window.localStorage.getItem("AuthData");
-                const authData = JSON.parse(authDataSerialized);
-                if (authDataSerialized) {
-                    headers.Authorization = `${authData?.token?.type} ${authData?.token?.token}`;
-                }
-                this.headers = headers;
-                return headers;
-            }
-
-        } catch (error) {
+  request: Axios;
+  headers: AxiosRequestHeaders;
+  loadRequest() {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    this.loadHeaders();
+    this.request = axios.create({
+      baseURL: baseUrl,
+      headers: this.headers,
+    });
+  }
+  loadHeaders() {
+    try {
+      console.log(this.headers);
+      if (this.headers && Object.keys(this.headers).length) return this.headers;
+      const headers: AxiosRequestHeaders = {};
+      if (typeof window !== "undefined") {
+        const authDataSerialized = window.localStorage.getItem("AuthData");
+        const authData = JSON.parse(authDataSerialized);
+        if (authDataSerialized) {
+          headers.Authorization = `${authData?.token?.type} ${authData?.token?.token}`;
         }
-    }
+        this.headers = headers;
+        return headers;
+      }
+    } catch (error) {}
+  }
 }
 
 export default BaseApi;
