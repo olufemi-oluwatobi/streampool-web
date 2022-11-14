a5afcc43118d43d9ee48b5706ef77f7ef01798db
unknown <olufemioluwatobi1996@gmail.com>
2022-11-14 11:07:56 +0100

    fix meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-    return (
-        <Html>
-            <Head >
-
-                <link href="/static/fonts/PolySans/style.css" rel="stylesheet" />
-                <link href="/static/fonts/Lato/stylesheet.css" rel="stylesheet" />
-                <link rel="shortcut icon" href="/static/favicon.png" />
-            </Head>
-            <body className="dark:bg-gray-800">
-                <Main />
-                <NextScript />
-                <script src="/static/scripts/index.js" />
-            </body>
-        </Html>
-    )
+  return (
+    <Html>
+      <Head>
+        <link href="/static/fonts/PolySans/style.css" rel="stylesheet" />
+        <link href="/static/fonts/Lato/stylesheet.css" rel="stylesheet" />
+        <link rel="shortcut icon" href="/static/favicon.png" />
+        <meta property="og:title" content="Streamcel" />
+        <meta property="og:url" content="https://www.streamcel.com" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/drda29q8x/image/upload/v1668419884/Artboard_1_copy_swvwml.png"
+        />
+        <meta name="twitter:title" content="Streamcel " />
+        <meta
+          name="twitter:description"
+          content="Stream more at a budget by sharing subscriptions to your favourite streaming services with other people"
+        />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/drda29q8x/image/upload/v1668419884/Artboard_1_copy_swvwml.png"
+        />
+        <meta name="twitter:card" content="summary"></meta>
+      </Head>
+      <body className="dark:bg-gray-800">
+        <Main />
+        <NextScript />
+        <script src="/static/scripts/index.js" />
+      </body>
+    </Html>
+  );
 }
